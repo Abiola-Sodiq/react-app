1d3f0127da30c28143f93d6c8abb393c3b9bb4d6
abiola-sodiq <rasababe01@gmail.com>
2023-05-31 15:47:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import "./index.css"
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      backgroundColor: "black"
+    }
+  }
+
+  // method used to update the bckground color
+updateBackgroundColor(color){
+  this.setState(this.state = {
+    backgroundColor: color
+  })
+}
+
+  render() {
+    return (
+      <div style={{
+        height: "100svh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: this.state.backgroundColor
+      }}>
+        <input type="text" style={{borderColor: "blue"}} onChange={(e) => this.updateBackgroundColor(e.target.value)}/>
+
+      </div>
+    )
+  }
+}
+
+export default App
